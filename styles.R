543446a1358c82a12a3fea847dda67964bc82e89
heike-s <heike.schuler@yahoo.de>
2024-03-06 17:19:13 -0500

    F1b added

--- styles.R
+++ styles.R
@@ -4,18 +4,18 @@ library(ggplot2)
 
 # General Plot Theme ----
 
-plot_theme <- theme(axis.title=element_text(size=25,color='gray30'),
+plot_theme <- theme(axis.title=element_text(size=15,color='gray30'),
                     axis.line=element_line(color='gray30'),
-                    axis.text.y=element_text(size=20,color='gray30'),
-                    axis.text.x=element_text(size=20,color='gray30'),
-                    legend.text=element_text(size=20),
+                    axis.text.y=element_text(size=15,color='gray30'),
+                    axis.text.x=element_text(size=15,color='gray30'),
+                    legend.text=element_text(size=15),
                     legend.key.size=unit(2,'mm'),
                     panel.background=element_blank(),
                     panel.grid.major.y = element_blank(),
                     legend.position = 'none',
                     plot.margin = margin(0.5,0.5,0.5,0.5,'cm'),
                     strip.background = element_blank(),
-                    strip.text = element_text(size=20,color='gray30'))
+                    strip.text = element_text(size=15,color='gray30'))
 
 # Color palettes ----
 
